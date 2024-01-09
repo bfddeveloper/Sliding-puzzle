385beb2368fa6cc8e9f461ee51e0cc53fd152e94
bfddeveloper <bdicksondev@gmail.com>
2024-01-09 09:35:36 -0600

    Initial Commit

---
+++ Sliding puzzle/Sliding_puzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sliding_puzzleApp.swift
+//  Sliding puzzle
+//
+//  Created by Brody Dickson on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Sliding_puzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
